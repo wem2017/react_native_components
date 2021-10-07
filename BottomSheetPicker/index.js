4d82d9358b69a165178d9fc07449904b780dd3de
dung.huynh1 <dung.huynh1@mservice.com.vn>
2021-10-08 06:46:03 +0700

    no message

--- BottomSheetPicker/index.js
+++ BottomSheetPicker/index.js
@@ -1,140 +1,206 @@
-import React, {useRef, useState, forwardRef, useImperativeHandle} from 'react';
+import React, {
+  useRef,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useMemo,
+  memo,
+} from 'react';
+import {
+  BottomSheetModal,
+  BottomSheetFlatList,
+  BottomSheetBackdrop,
+} from '@gorhom/bottom-sheet';
 import {View, TouchableOpacity} from 'react-native';
+import {useTranslation} from 'react-i18next';
 import PropTypes from 'prop-types';
-import {Text, SearchInput, Icon, BottomSheetFlatList, Image} from '@components';
-import {Styles, useTheme} from '@configs';
+import {Text, SearchInput, Icon, SafeAreaView, Image} from '@components';
+import {Styles, useTheme, Images} from '@configs';
 import styles from './styles';
 
-const Index = forwardRef((props, ref) => {
-  const {colors} = useTheme();
-  const bottomSheetRef = useRef(null);
-  useImperativeHandle(ref, () => bottomSheetRef.current);
+const MemoInput = memo(({value, onChangeText}) => {
+  const [keyword, setKeyword] = useState(value);
 
-  const [keyword, setKeyword] = useState();
-  const [selected, setSelected] = useState();
+  return (
+    <SearchInput
+      type="bottomsheet"
+      value={keyword}
+      onChangeText={val => {
+        setKeyword(val);
+        onChangeText(val);
+      }}
+    />
+  );
+});
 
-  const {component, title, data, initHeight} = props;
+const MemoList = memo(
+  forwardRef(({data}, ref) => {
+    const {colors} = useTheme();
+    const [list, setList] = useState(data);
+    const [selected, setSelected] = useState();
 
-  /**
-   * on change search
-   */
-  const onSearch = value => {
-    setKeyword(value);
-  };
+    useImperativeHandle(ref, () => ({
+      onChangeText: value => {
+        if (value) {
+          setList(
+            data.filter(item =>
+              item.title?.toUpperCase().includes(value.toUpperCase()),
+            ),
+          );
+        } else {
+          setList(data);
+        }
+      },
+      selected,
+    }));
 
-  /**
-   * render item
-   * @param {*} {item}
-   * @return {*}
-   */
-  const renderItem = ({item}) => {
-    let style = {};
-    let trailing;
-    if (item.value === selected?.value) {
-      style = {
-        backgroundColor: colors.primary + '1A',
-      };
-      trailing = <Icon name="check" size={24} color={colors.primary} />;
-    }
-    return (
-      <TouchableOpacity
-        style={[styles.item, style]}
-        onPress={() => setSelected(item)}>
-        {item.icon && (
-          <Image
-            style={styles.icon}
-            source={{uri: item.icon}}
-            resizeMode="contain"
-          />
-        )}
-        <Text typography="h4" style={Styles.flex}>
-          {item.title}
-        </Text>
-        {trailing}
-      </TouchableOpacity>
-    );
-  };
+    /**
+     * render item
+     * @param {*} {item}
+     * @return {*}
+     */
+    const renderItem = ({item}) => {
+      let style = {};
+      let trailing;
+      if (item.value === selected?.value) {
+        style = {
+          backgroundColor: colors.primary + '1A',
+        };
+        trailing = <Icon name="check" size={24} color={colors.primary} />;
+      }
+      return (
+        <TouchableOpacity
+          style={[styles.item, style]}
+          onPress={() => setSelected(item)}>
+          {item.icon && (
+            <Image
+              style={styles.icon}
+              source={{uri: item.icon}}
+              resizeMode="contain"
+            />
+          )}
+          <Text typography="h4" style={Styles.flex}>
+            {item.title}
+          </Text>
+          {trailing}
+        </TouchableOpacity>
+      );
+    };
 
-  if (component === 'BottomSheetFlatList') {
     return (
       <BottomSheetFlatList
-        ref={bottomSheetRef}
-        initHeight={initHeight}
         contentContainerStyle={Styles.paddingVertical8}
-        header={
-          <>
-            <View
-              style={[styles.contentTitle, {borderBottomColor: colors.border}]}>
-              <TouchableOpacity>
-                <Text
-                  typography="title"
-                  weight="bold"
-                  style={styles.textButton}>
-                  Huỷ
-                </Text>
-              </TouchableOpacity>
-              <Text typography="h4" weight="bold" style={styles.titleText}>
-                {title}
-              </Text>
-              <TouchableOpacity>
-                <Text
-                  typography="title"
-                  weight="bold"
-                  color="primary"
-                  style={styles.textButton}>
-                  Lưu
-                </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={styles.searchContent}>
-              <SearchInput
-                type="bottomsheet"
-                value={keyword}
-                onChangeText={onSearch}
-              />
-            </View>
-          </>
-        }
-        data={data}
+        data={list}
         keyExtractor={item => item?.value}
         renderItem={renderItem}
       />
     );
-  }
+  }),
+);
+
+const Index = forwardRef((props, ref) => {
+  const {colors} = useTheme();
+  const bottomSheetRef = useRef(null);
+  const listRef = useRef(null);
+  const {t} = useTranslation();
+
+  useImperativeHandle(ref, () => bottomSheetRef.current);
+
+  const {search, title, data, initHeight, onSelect} = props;
+
+  const snapPoints = useMemo(() => [initHeight, '100%'], [initHeight]);
+
+  /**
+   * on change search
+   */
+  const onChangeText = value => {
+    listRef.current?.onChangeText(value);
+  };
+
+  return (
+    <BottomSheetModal
+      ref={bottomSheetRef}
+      backgroundStyle={{backgroundColor: colors.card}}
+      snapPoints={snapPoints}
+      keyboardBehavior={'interactive'}
+      keyboardBlurBehavior={'none'}
+      stackBehavior="push"
+      handleComponent={() => (
+        <SafeAreaView edges={['right', 'top', 'left']}>
+          <View style={styles.indicatorContainer}>
+            <View style={[styles.indicator, {backgroundColor: colors.card}]} />
+          </View>
+          <View
+            style={[styles.contentTitle, {borderBottomColor: colors.border}]}>
+            <TouchableOpacity onPress={() => bottomSheetRef.current.dismiss()}>
+              <Text typography="title" weight="bold" style={styles.textButton}>
+                {t('cancel')}
+              </Text>
+            </TouchableOpacity>
+            <Text typography="h4" weight="bold" style={styles.titleText}>
+              {title}
+            </Text>
+            <TouchableOpacity
+              onPress={() => {
+                if (listRef.current?.selected) {
+                  onSelect(listRef.current?.selected);
+                  bottomSheetRef.current.dismiss();
+                }
+              }}>
+              <Text
+                typography="title"
+                weight="bold"
+                color="primary"
+                style={styles.textButton}>
+                {t('save')}
+              </Text>
+            </TouchableOpacity>
+          </View>
+          {search && (
+            <View style={styles.searchContent}>
+              <MemoInput type="bottomsheet" onChangeText={onChangeText} />
+            </View>
+          )}
+        </SafeAreaView>
+      )}
+      backdropComponent={backdropProps => (
+        <BottomSheetBackdrop
+          disappearsOnIndex={-1}
+          appearsOnIndex={0}
+          {...backdropProps}
+        />
+      )}
+      enablePanDownToClose={true}>
+      <MemoList ref={listRef} data={data} />
+    </BottomSheetModal>
+  );
 });
 
 export default Index;
 
 Index.propTypes = {
-  component: PropTypes.string,
+  search: PropTypes.bool,
   title: PropTypes.string,
   data: PropTypes.array,
   initHeight: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  onCancel: PropTypes.func,
-  onSave: PropTypes.func,
+  onSelect: PropTypes.func,
 };
 
 Index.defaultProps = {
-  component: 'BottomSheetFlatList',
+  search: true,
   title: 'Chọn ngôn ngữ',
   data: [
     {
-      value: 'VN',
+      value: 'vi',
       title: 'Việt Nam',
-      icon: 'https://img.mservice.io/momo_app_v2/new_version/growth_activation/img/vn.png',
+      icon: Images.vi,
     },
     {
-      value: 'AF',
-      title: 'Afghanistan',
-      icon: 'https://img.mservice.io/momo_app_v2/new_version/growth_activation/img/af.png',
-    },
-    {
-      value: 'AL',
-      title: 'Albania',
-      icon: 'https://img.mservice.io/momo_app_v2/new_version/growth_activation/img/al.png',
+      value: 'en',
+      title: 'English',
+      icon: Images.en,
     },
   ],
   initHeight: '50%',
-  onCancel: () => {},
-  onSave: item => {},
+  onSelect: value => {},
 };
