e97089bb9f548a87a5c77ae0c7c93ed9f143149a
Dung Huynh <dung.huynh1@mservice.com.vn>
2021-12-29 23:55:31 +0700

    no message

--- IconButton/index.js
+++ IconButton/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import PropTypes from 'prop-types';
-import {Icon} from '@components';
-import {useTheme, Colors} from '@configs';
+import {useTheme} from '@configs';
 import styles from './styles';
 
 export default function IconButton(props) {
   const {theme} = useTheme();
-  const {style, iconStyle, type, size, shape, name, source} = props;
+  const {style, type, size, shape, children} = props;
 
   /**
    * export size style
@@ -21,21 +20,6 @@ export default function IconButton(props) {
 
       default:
         return styles.large;
-    }
-  };
-
-  /**
-   * export size icon
-   */
-  const getIconSize = () => {
-    switch (size) {
-      case 'large':
-        return 32;
-      case 'small':
-        return 16;
-
-      default:
-        return 16;
     }
   };
 
@@ -64,24 +48,6 @@ export default function IconButton(props) {
     }
   };
 
-  /**
-   * export icon color
-   */
-  const getIconColor = () => {
-    switch (type) {
-      case 'primary':
-        return Colors.white;
-      case 'secondary':
-        return theme.colors.text;
-      case 'outline':
-        return theme.colors.primary;
-      case 'disable':
-        return theme.colors.textSecondary;
-      default:
-        return Colors.white;
-    }
-  };
-
   const buttonStyle = StyleSheet.flatten([
     getSizeStyle(),
     getTypeStyle(),
@@ -94,33 +60,21 @@ export default function IconButton(props) {
       {...props}
       style={buttonStyle}
       disabled={type === 'disable'}>
-      <Icon
-        name={name ?? 'help-circle'}
-        color={getIconColor()}
-        size={getIconSize()}
-        type={source}
-        style={iconStyle}
-      />
+      {children}
     </TouchableOpacity>
   );
 }
 
 IconButton.propTypes = {
-  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-  iconStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   type: PropTypes.oneOf(['primary', 'secondary', 'outline', 'disable']),
   size: PropTypes.oneOf(['large', 'small']),
   shape: PropTypes.oneOf(['circle', 'rectangle']),
-  name: PropTypes.string,
-  source: PropTypes.string,
+  children: PropTypes.node.isRequired,
 };
 
 IconButton.defaultProps = {
-  style: {},
-  iconStyle: {},
-  type: 'primary',
-  size: 'large',
+  type: 'secondary',
+  size: 'small',
   shape: 'circle',
-  name: null,
-  source: null,
+  children: <View />,
 };
