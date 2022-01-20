208e64a947b03b1b4444822c8e8637afe5795272
Dung Huynh <dung.huynh1@mservice.com.vn>
2022-01-21 06:53:36 +0700

    no message

--- ListItem/index.js
+++ ListItem/index.js
@@ -54,7 +54,11 @@ export default function ListItem(props) {
   const buildSubTitle = () => {
     if (subtitle && size !== 16) {
       return (
-        <Text typography="subtitle" type="secondary" style={styles.subTitle}>
+        <Text
+          typography="subtitle"
+          type="secondary"
+          style={styles.subTitle}
+          numberOfLines={1}>
           {subtitle}
         </Text>
       );
